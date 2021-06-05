922c83878bb9b5b608a03bf1760442bc3998430c
eelsvek <kev.sunlee@gmail.com>
2021-06-05 16:23:03 -0700

    chore: remove initial files

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>Test: {{ name }}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const name = ref('Kevin');
+
+    return {
+      name,
+    };
+  },
+};
 </script>
 
 <style>
