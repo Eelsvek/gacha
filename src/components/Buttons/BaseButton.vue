372c254f3833b6a38e3cbe58c598c28900b8302e
eelsvek <kev.sunlee@gmail.com>
2021-06-06 12:52:06 -0700

    feat: BaseButton
    adds support for variant prop

--- src/components/Buttons/BaseButton.vue
+++ src/components/Buttons/BaseButton.vue
@@ -1,13 +1,30 @@
 <template>
-  <button class="px-2 py-1">
+  <button class="px-3 py-1 rounded-sm text-sm" :class="buttonClass">
     <slot />
   </button>
 </template>
 
 <script>
+const buttonTheme = (variant) => {
+  switch (variant) {
+    case 'primary':
+      return 'bg-indigo-600 text-white';
+    default:
+      return '';
+  }
+};
+
 export default {
-  setup() {
-    return {};
+  props: {
+    variant: {
+      type: String,
+      default: 'primary',
+    },
+  },
+  setup(props) {
+    return {
+      buttonClass: buttonTheme(props.variant),
+    };
   },
 };
 </script>
