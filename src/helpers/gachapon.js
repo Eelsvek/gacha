579f689856ba5e6e1bfdab48dc0af0b9e4171d00
eelsvek <kev.sunlee@gmail.com>
2021-06-10 23:55:11 -0700

    feature(gachapon): helper functions + tests

--- src/helpers/gachapon.js
+++ src/helpers/gachapon.js
@@ -1,20 +1,72 @@
-import characters from '@/data/characters'
+import CHARACTER_DATA from '@/data/characters';
 
-const ProbabilityMap = Object.freeze({
+export const ProbabilityMap = Object.freeze({
   5: .5,
   4: 1,
   3: 9,
   2: 20,
   1: 80,
-})
+});
+
+export const ULTRA_RARE_PERCENTAGE = ProbabilityMap[5];
+export const SUPER_RARE_PERCENTAGE = ProbabilityMap[4];
+export const RARE_PERCENTAGE = ProbabilityMap[3];
+export const UNCOMMON_PERCENTAGE = ProbabilityMap[2];
+export const COMMON_PERCENTAGE = ProbabilityMap[1];
 
 const getCharacter = () => {
-  console.log('Probability map', ProbabilityMap)
-  console.log('characters', characters)
-}
+  const characters = mapCharacterRarity(CHARACTER_DATA);
+
+  console.log('math.random', Math.random());
+  // console.log('characterChance', characters);
+
+  return characters[0];
+};
+
+export const selectCharacter = (characters, percentage) => {
+  let character;
+  if (percentage <= ULTRA_RARE_PERCENTAGE) {
+    const ultraRareCharacters = filterCharactersByChance(characters, ULTRA_RARE_PERCENTAGE);
+    character = selectRandomlyFrom(ultraRareCharacters);
+  } else if (percentage <= SUPER_RARE_PERCENTAGE) {
+    const superRareCharacters = filterCharactersByChance(characters, SUPER_RARE_PERCENTAGE);
+    character = selectRandomlyFrom(superRareCharacters);
+  } else if (percentage <= RARE_PERCENTAGE) {
+    const rareCharacters = filterCharactersByChance(characters, RARE_PERCENTAGE);
+    character = selectRandomlyFrom(rareCharacters);
+  } else if (percentage <= UNCOMMON_PERCENTAGE) {
+    const uncommonCharacters = filterCharactersByChance(characters, UNCOMMON_PERCENTAGE);
+    character = selectRandomlyFrom(uncommonCharacters);
+  } else if (percentage <= COMMON_PERCENTAGE) {
+    const commonCharacters = filterCharactersByChance(characters, COMMON_PERCENTAGE);
+    character = selectRandomlyFrom(commonCharacters);
+  } else {
+    const commonCharacters = filterCharactersByChance(characters, COMMON_PERCENTAGE);
+    character = selectRandomlyFrom(commonCharacters);
+  }
+
+  return character;
+};
+
+const filterCharactersByChance = (characters, chance) => {
+  return characters.filter(c => c.chance === chance);
+};
+
+const selectRandomlyFrom = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+export const mapCharacterRarity = (characters) => {
+  return characters.map(c => {
+    return {
+      ...c,
+      chance: ProbabilityMap[c.rarity]
+    };
+  });
+};
 
 export const singleRoll = () => {
-  return getCharacter()
-}
+  return getCharacter();
+};
 
-export default null
+export default null;
--- src/helpers/gachapon.test.js
+++ src/helpers/gachapon.test.js
@@ -1,9 +1,60 @@
-import { singleRoll } from './gachapon'
+import characters from '@/data/characters';
+import { singleRoll, selectCharacter, mapCharacterRarity, ULTRA_RARE_PERCENTAGE, SUPER_RARE_PERCENTAGE, RARE_PERCENTAGE, UNCOMMON_PERCENTAGE, COMMON_PERCENTAGE } from './gachapon';
+
+
+const testCharacters = mapCharacterRarity(characters);
 
 describe('gachapon.js', () => {
   describe('singleRoll', () => {
-    const result = singleRoll()
+    test('it should return a character', () => {
+      const result = singleRoll();
 
-    expect(result).toBeTruthy()
-  })
-})
+      expect(result).toBeTruthy();
+    });
+  });
+
+  describe.only('selectCharacter', () => {
+    const tests = [
+      {
+        name: 'should return an ultra rare character',
+        input: testCharacters,
+        expected: ULTRA_RARE_PERCENTAGE
+      },
+      {
+        name: 'should return a super rare character',
+        input: testCharacters,
+        expected: SUPER_RARE_PERCENTAGE,
+        shouldThrow: true
+      },
+      {
+        name: 'should return a rare character',
+        input: testCharacters,
+        expected: RARE_PERCENTAGE,
+        shouldThrow: true
+      },
+      {
+        name: 'should return an uncommon character',
+        input: testCharacters,
+        expected: UNCOMMON_PERCENTAGE
+      },
+      {
+        name: 'should return a common character',
+        input: testCharacters,
+        expected: COMMON_PERCENTAGE
+      }
+    ];
+
+    tests.forEach(t => {
+      test(t.name, () => {
+        const expectation = expect(selectCharacter(t.input, t.expected));
+
+        if (t.shouldThrow) {
+          console.log('???', expectation);
+          expectation.to.throw();
+        } else {
+          expectation.to.eq(t.expected);
+        }
+      });
+    });
+  });
+});
